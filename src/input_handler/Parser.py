68aaa2101474438202319c7a0717a0c3d61184f0
tms-gvd <thomas.gaviard@gmail.com>
2022-11-12 13:30:55 +0100

    fix: small changes

--- src/input_handler/Parser.py
+++ src/input_handler/Parser.py
@@ -15,20 +15,21 @@ class Parser:
 
     def parse(self):
 
-        self.prob = {}
         lines = self.input.split("\n")
 
         # get the main parameters L, G, S, B, F, N
         l, g, s, b, f, n = parse_main_params(lines[0])
-        self.prob["L"] = l
-        self.prob["G"] = g
-        self.prob["S"] = s
-        self.prob["B"] = b
-        self.prob["F"] = f
-        self.prob["N"] = n
+        self.main_params = {
+            "L": l,
+            "G": g,
+            "S": s,
+            "B": b,
+            "F": f,
+            "N": n
+        }
 
         # get the services infos on the lines N°1 to S
-        self.prob["services"] = parse_services(lines[1:s+1])
+        self.services = parse_services(lines[1:s+1])
 
         # get the features infos on the lines S+1 to the end, by group of 2
-        self.prob["features"] = parse_features(lines[s+1:])
+        self.features = parse_features(lines[s+1:])
