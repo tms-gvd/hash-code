0016559e89b4c5aa46122e2d5993d4db71337df4
tjsimpletg <58075318+tjsimpletg@users.noreply.github.com>
2022-11-13 18:01:18 +0000

    Deploying to gh-pages from @ tms-gvd/hash-code@90780e2f851c9974fdd68e64cf1b395792834931 🚀

--- tests/test_output_handler.py
+++
@@ -1,7 +0,0 @@
-"""Check the validity of the solutions in solutions folder"""
-
-from src.output_handler.ParseSolution import ParseSolution
-
-def check_solution(sol_name:str, inp_name:str):
-
-    pass
